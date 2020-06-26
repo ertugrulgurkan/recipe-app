1c4f164662f264689d648d07e9f16d24f3ff4d31
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-06-26 14:50:47 +0300

    Initial commit

---
+++ src/test/java/com/ertugrul/recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ertugrul.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
