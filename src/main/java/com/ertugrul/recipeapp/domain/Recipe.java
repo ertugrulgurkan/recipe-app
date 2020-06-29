ab4b56cf9ff13f16a0ecd73848529d220aa2a550
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-06-29 14:59:09 +0300

    adding a many to many relationship.

--- src/main/java/com/ertugrul/recipeapp/domain/Recipe.java
+++ src/main/java/com/ertugrul/recipeapp/domain/Recipe.java
@@ -29,6 +29,13 @@ public class Recipe {
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
@@ -126,5 +133,13 @@ public class Recipe {
 
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
