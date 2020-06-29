8a2e95d8e23a9d1bd278dc223f7dc5e1241e39bf
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-06-29 18:00:12 +0300

    Adding Query Method...

--- src/main/java/com/ertugrul/recipeapp/controllers/IndexController.java
+++ src/main/java/com/ertugrul/recipeapp/controllers/IndexController.java
@@ -1,13 +1,34 @@
 package com.ertugrul.recipeapp.controllers;
 
+import com.ertugrul.recipeapp.domain.Category;
+import com.ertugrul.recipeapp.domain.UnitOfMeasure;
+import com.ertugrul.recipeapp.repositories.CategoryRepository;
+import com.ertugrul.recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is: " + categoryOptional.get().getId());
+        System.out.println("UOM ID is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
--- src/main/java/com/ertugrul/recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/ertugrul/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,10 @@ package com.ertugrul.recipeapp.repositories;
 import com.ertugrul.recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure,Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
+
 }
