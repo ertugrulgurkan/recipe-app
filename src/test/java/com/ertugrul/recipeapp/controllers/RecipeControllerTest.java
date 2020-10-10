ad9f760d24a765e9a11ca912fd78673f9c91cde5
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-10-10 21:55:42 +0300

    adding exception handler

--- src/test/java/com/ertugrul/recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/com/ertugrul/recipeapp/controllers/RecipeControllerTest.java
@@ -108,7 +108,9 @@ public class RecipeControllerTest {
 
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
-        mockMvc.perform(get("/recipe/1/show")).andExpect(status().isNotFound());
+        mockMvc.perform(get("/recipe/1/show"))
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("404error"));
     }
 
 }
